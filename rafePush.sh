b08814a0ce20ac939882224f46d9b410c837271a
EsinShadrach <esinshadrachrafe@gmail.com>
2023-07-21 20:57:17 +0100

    Updated rafe_push

--- rafePush.sh
+++ rafePush.sh
@@ -1,12 +1,12 @@
 rafe_push() {
-  git add .
-  echo "ADDED GIT"
-  echo "Enter a commit message > "
-  read commit_message
-  git commit -m "$commit_message"
-  echo "ADDED COMMIT MESSAGE: $commit_message"
-  git status
-  git push origin main
-  echo "PUSHED"
+	git add .
+	echo "Added Git"
+	echo -n "Enter commit message > " 
+       	read -r commit_message
+  	git commit -m "$commit_message"
+  	echo "Added commit message: $commit_message"
+  	git status
+	git push origin main
+  	echo "Pushed"
 }
 
